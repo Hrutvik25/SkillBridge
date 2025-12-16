5ddd63f20bf865da7c51f45230eb7f4873b943cf
Hrutvik25 <rutvikwayase@gmail.com>
2025-12-16 16:36:48 +0530

    New Update

--- apps/frontend/src/components/InstagramFeed.tsx
+++ apps/frontend/src/components/InstagramFeed.tsx
@@ -14,7 +14,7 @@ export default function InstagramFeed() {
   return (
     <div className="w-full overflow-hidden rounded-xl shadow-md bg-background">
       <iframe
-        src="http://lightwidget.com/widgets/6fc67deb8d0b5c9e997d87b0446d5c90.html"
+        src="https://lightwidget.com/widgets/6fc67deb8d0b5c9e997d87b0446d5c90.html"
         scrolling="no"
         allowTransparency={true}
         className="lightwidget-widget w-full border-0 overflow-hidden"
